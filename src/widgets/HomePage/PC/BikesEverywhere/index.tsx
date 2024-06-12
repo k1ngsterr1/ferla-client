187a579b2d9baaac3cf88172b8fee6b2bbbc30dc
Ruslan Makhmatov <103198805+k1ngsterr1@users.noreply.github.com>
2024-06-13 01:44:27 +0600

    --FIXED

--- src/widgets/HomePage/PC/BikesEverywhere/index.tsx
+++ src/widgets/HomePage/PC/BikesEverywhere/index.tsx
@@ -6,15 +6,12 @@ import { setComponentId } from "../../../../redux/idSlice";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { ImageForm } from "@entities/ClientComponents/ImageForm";
 
-import bike01 from "@assets/webp/pc/bike_01_pc.webp";
-import bike02 from "@assets/webp/pc/bike_02_pc.webp";
-import bike03 from "@assets/webp/pc/bike_03_pc.webp";
-import bike04 from "@assets/webp/pc/bike_04_pc.webp";
-
 import styles from "./styles.module.scss";
 import { useFetchContent } from "@shared/lib/hooks/useFetchContent";
 
 export const BikesEverywhere = () => {
+  const dispatch = useDispatch();
+  const componentId = useSelector((state: any) => state.id.componentId);
   const [imageFormVisible, setImageFormVisible] = useState<boolean>(false);
 
   const handleImageFormVisible = (e: React.SyntheticEvent) => {
@@ -39,13 +36,9 @@ export const BikesEverywhere = () => {
             className={styles.everywhere_block__upper__heading}
             id="editable_ferla-bikes_89d6700c-288d-46c6-b463-60aae8b1b830_25"
           >
-            Ferla Bikes{" "}
-            <strong
-              className="text-red"
-              id="editable_ferla-bikes_89d6700c-288d-46c6-b463-60aae8b1b830_22"
-            >
-              Everywhere
-            </strong>
+            {content && content["24"]
+              ? content["24"].value
+              : "Content Not Found"}
           </h4>
           <div className="flex flex-col items-end">
             <p
@@ -66,22 +59,18 @@ export const BikesEverywhere = () => {
         </div>
         <div className={styles.everywhere_block__photos}>
           <img
-            src={bike01}
             id="editable_ferla-bikes_89d6700c-288d-46c6-b463-60aae8b1b830_27"
             src={
               content && content["26"]
                 ? content["26"].value
                 : "Content Not Found"
             }
-            id="editable_ferla-bikes_89d6700c-288d-46c6-b463-60aae8b1b830_24"
             onClick={handleImageFormVisible}
             className={styles.everywhere_block__photos__photo}
             alt="Ferla Bike"
           />
           <img
             id="editable_ferla-bikes_89d6700c-288d-46c6-b463-60aae8b1b830_28"
-            src={bike02}
-            id="editable_ferla-bikes_89d6700c-288d-46c6-b463-60aae8b1b830_25"
             src={
               content && content["27"]
                 ? content["27"].value
@@ -93,8 +82,6 @@ export const BikesEverywhere = () => {
           />
           <img
             id="editable_ferla-bikes_89d6700c-288d-46c6-b463-60aae8b1b830_29"
-            src={bike03}
-            id="editable_ferla-bikes_89d6700c-288d-46c6-b463-60aae8b1b830_26"
             src={
               content && content["28"]
                 ? content["28"].value
@@ -106,8 +93,6 @@ export const BikesEverywhere = () => {
           />
           <img
             id="editable_ferla-bikes_89d6700c-288d-46c6-b463-60aae8b1b830_30"
-            src={bike04}
-            id="editable_ferla-bikes_89d6700c-288d-46c6-b463-60aae8b1b830_27"
             src={
               content && content["29"]
                 ? content["29"].value
