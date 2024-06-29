e4c7f465d0817e8e04370d2a591551bd80cce504
k1ngsterr1 <ruslanmakhmatov@gmail.com>
2024-06-29 11:45:39 +0500

    --ADDED AUTH CONTEXT

--- src/shared/lib/hooks/useUserData.ts
+++ src/shared/lib/hooks/useUserData.ts
@@ -1,14 +1,49 @@
 import { useState, useEffect } from "react";
 
-export const useUserData = () => {
-  const [userData, setUserData] = useState(null);
+interface IUserData {
+  username: string | null;
+  email: string | null;
+  accessToken: string | null;
+}
+
+interface UserDataHookResult {
+  userData: IUserData;
+  isLoading: boolean;
+}
+
+export const useUserData = (): UserDataHookResult => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [userData, setUserData] = useState<IUserData>(() => {
+    const data = localStorage.getItem("userData");
+    return data
+      ? JSON.parse(data)
+      : { username: null, email: null, accessToken: null };
+  });
 
   useEffect(() => {
-    const data = localStorage.getItem("userData");
-    if (data) {
-      setUserData(JSON.parse(data));
-    }
+    const handleStorageChange = (event: StorageEvent) => {
+      if (event.key === "userData") {
+        try {
+          setIsLoading(true);
+          const newData = event.newValue ? JSON.parse(event.newValue) : null;
+          setUserData(newData);
+        } catch (error) {
+          console.error(
+            "Error parsing updated user data from localStorage",
+            error
+          );
+        } finally {
+          setIsLoading(false);
+        }
+      }
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
   }, []);
 
-  return { userData };
+  return { userData, isLoading };
 };
---
+++ src/widgets/Contexts/AuthProvider/index.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { createContext, useState, useEffect, useContext } from "react";
+import { jwtDecode } from "jwt-decode";
+import { useUserData } from "@shared/lib/hooks/useUserData";
+import { redirect } from "react-router-dom";
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+}
+
+const AuthContext = createContext<AuthContextType>({ isAuthenticated: false });
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const { userData, isLoading } = useUserData();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkTokenExpiration = (token) => {
+      try {
+        const { exp } = jwtDecode(token);
+        return exp * 1000; // Convert to milliseconds
+      } catch (error) {
+        return null;
+      }
+    };
+
+    const verifyToken = async () => {
+      if (isLoading) return;
+
+      const token = userData?.accessToken;
+
+      if (token) {
+        const expirationTime = checkTokenExpiration(token);
+        if (expirationTime) {
+          const timeUntilExpiration = expirationTime - Date.now();
+          setIsAuthenticated(true); // Set authenticated initially if token is present and valid
+
+          const timeout = setTimeout(() => {
+            localStorage.removeItem("accessToken"); // Remove expired token
+            setIsAuthenticated(false); // Set authenticated to false on token expiration
+            redirect("/ferla/login"); // Redirect to login page
+          }, timeUntilExpiration);
+
+          return () => clearTimeout(timeout);
+        }
+      } else {
+        setIsAuthenticated(false); // No token or expired token
+        redirect("/ferla/login"); // Redirect to login page
+      }
+    };
+
+    verifyToken(); // Call async function to verify token
+  }, [userData?.accessToken, router, isLoading]);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
